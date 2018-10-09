a74fb51c6d5fdad7f6899709f91aa6afb523cd2e
Caleb Coffie <ccoffie@localhost.localdomain>
2018-10-08 22:02:23 -0500

    Commiting to backup

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.scss';
+
+class Index extends React.Component {
+    render() {
+        return <h1>React coming soon...</h1>;
+    }
+}
+export default Index;
